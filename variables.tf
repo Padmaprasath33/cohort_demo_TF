65635726ab827b0807a663e4d4d737d3cd6837a0
Padmaprasath <padmaprasath33@gmail.com>
2023-12-11 19:13:26 +0530

    Added ECS config

--- variables.tf
+++ variables.tf
@@ -8,6 +8,11 @@ variable "datadog_api_key" {
   default     = "7f14916484a1f8410026a47e2c2f97c6"
 }
 
+variable "datadog_app_key" {
+  description = "Datadog App key"
+  default     = "e168ad50d11be536078cb9861f7a0351cc9db0ee"
+}
+
 variable "resource_tags" {
   description = "Tags to set for all resources"
   type        = map(string)
--- versions.tf
+++ versions.tf
@@ -27,6 +27,7 @@ provider "aws" {
 
 provider "datadog" {
   api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
 }
 
 
