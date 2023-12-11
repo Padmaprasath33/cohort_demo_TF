056bbb07bae8c1fede5751d23f2f61188cb810a0
Padmaprasath <padmaprasath33@gmail.com>
2023-12-11 21:03:28 +0530

    Added ECS config

--- datadog.tf
+++ datadog.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
 
    principals {
       type = "AWS"
-      identifiers = ["arn:aws:iam::500452054617:user/2191420"]
+      identifiers = ["arn:aws:iam::464622532012:root"]
    }
    condition {
       test = "StringEquals"
