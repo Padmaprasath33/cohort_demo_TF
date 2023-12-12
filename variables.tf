9c3f2139ab8f09e0ec587c1f812c9b0e004de2ca
Padmaprasath <padmaprasath33@gmail.com>
2023-12-12 15:47:13 +0530

    Added ECS config

--- variables.tf
+++ variables.tf
@@ -5,12 +5,14 @@ variable "region" {
 
 variable "datadog_api_key" {
   description = "Datadog Api key"
-  default     = "7f14916484a1f8410026a47e2c2f97c6"
+  //default     = "7f14916484a1f8410026a47e2c2f97c6"
+  //sensitive = true
 }
 
 variable "datadog_app_key" {
   description = "Datadog App key"
-  default     = "e168ad50d11be536078cb9861f7a0351cc9db0ee"
+  //default     = "e168ad50d11be536078cb9861f7a0351cc9db0ee"
+  //sensitive = true
 }
 
 variable "resource_tags" {
